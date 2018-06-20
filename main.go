7d9b8cd6d5d6d4fc0ec40198c1e8ae129ffd5730
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2018-06-20 16:27:54 +0300

    Write minimal command line client
    It takes in an input file (which is in oslo.policy format) and outputs
    either to standard output or to a file.

--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 
 	o2r "github.com/JAORMX/oslopolicy2rego/parser"
 )
 
 func main() {
-	sampleInput := `
-{
-	"secrets:get": "False:%(target.secret.project_id)s or (rule:creator and rule:reader)"
-}
-`
-	output, err := o2r.OsloPolicy2Rego(sampleInput)
+	var outputStream io.Writer
+
+	inputFile := flag.String("input", "", "Path to input oslo.policy file.")
+	outputFile := flag.String("output", "", "Path to input oslo.policy file.")
+
+	flag.Parse()
+
+	if *inputFile == "" {
+		panic("Must specify an input file.")
+	}
+
+	inputStream, err := ioutil.ReadFile(*inputFile)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(output)
+	inputString := string(inputStream)
+
+	if *outputFile == "" {
+		outputStream = os.Stdout
+	} else {
+		var err error
+		outputStream, err = os.OpenFile(*outputFile, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+	outputString, err := o2r.OsloPolicy2Rego(inputString)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprint(outputStream, outputString)
 }
